6cfe77fca34ee918880648315d52e52510084a90
Hadi Ghilas <hadi_ghilas@hotmail.fr>
2017-06-21 16:42:57 +0200

    insertion de l'api et de la boucle

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ var app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+var movie = [];
+
 app.get('/', function(req, res){
-  res.render('home');
+  request('https://api.themoviedb.org/3/discover/movie?api_key=a492894f4023ba237e9b72222795fbf1&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false', function(error, response, body){
+    body=JSON.parse(body);
+    res.render('home', {movie : body.results});
+  });
 });
 
 app.get('/home', function(req, res){
-  res.render('home');
+  request('https://api.themoviedb.org/3/discover/movie?api_key=a492894f4023ba237e9b72222795fbf1&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false', function(error, response, body){
+    body=JSON.parse(body);
+    res.render('home', {movie : body.results});
+  });
 });
 
 app.get('/contact', function(req, res){
