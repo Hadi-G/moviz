0c93c63ebd3ea026bacf54cd840e0a2ce5e05c20
Hadi Ghilas <hadi_ghilas@hotmail.fr>
2017-06-21 10:38:09 +0200

    integration du template et des routes

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.send('la page est chargée !');
+  res.render('home');
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact');
+});
+
+app.get('/review', function(req, res){
+  res.render('review');
+});
+
+app.get('/single', function(req, res){
+  res.render('single');
 });
 
 app.listen(8080, function () {
