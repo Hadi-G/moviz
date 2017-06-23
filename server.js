4d8918622c30cfa33338667e1270acb6471407f2
Hadi Ghilas <hadi_ghilas@hotmail.fr>
2017-06-23 13:49:26 +0200

    base de donnees et wish list

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var express  = require('express');
 var request  = require('request');
+var mongoose = require('mongoose');
+var session  = require('express-session');
+
+app.use(
+ session({
+  secret: 'a4f8071f-c873-4447-8ee2',
+  resave: false,
+  saveUninitialized: false,
+ })
+);
+
+var options = { server: { socketOptions: {connectTimeoutMS: 30000 } }};
+mongoose.connect('mongodb://hadi:zipang75@ds129532.mlab.com:29532/cinema',options, function(err) {
+  console.log(err);
+});
+
+var movieSchema = mongoose.Schema({
+  image : String,
+  title : String,
+  overview : String
+});
+
+var movieModel = mongoose.model('movie',movieSchema);
 
 var app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
-var movie = [];
 
 app.get('/', function(req, res){
   request('https://api.themoviedb.org/3/discover/movie?api_key=a492894f4023ba237e9b72222795fbf1&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false', function(error, response, body){
@@ -17,19 +38,35 @@ app.get('/', function(req, res){
 app.get('/home', function(req, res){
   request('https://api.themoviedb.org/3/discover/movie?api_key=a492894f4023ba237e9b72222795fbf1&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false', function(error, response, body){
     body=JSON.parse(body);
-    console.log(body.results);
-    console.log(body.results[0].id);
     res.render('home', {movie : body.results});
+  });
+  request('https://api.themoviedb.org/3/movie/'+req.query.id+'?api_key=a492894f4023ba237e9b72222795fbf1&language=fr-FR', function(error, response, single){
+    single=JSON.parse(single);
+    var favorite = new movieModel({
+      image : single.poster_path,
+      title : single.title,
+      overview : single.overview
+    });
+    favorite.save(function(error, verif){
+      console.log(verif);
+    });
+
   });
 });
 
 app.get('/contact', function(req, res){
+
   res.render('contact');
 });
 
 app.get('/review', function(req, res){
-  res.render('review');
+    movieModel.remove({_id:req.query.id}, function(error){
+      movieModel.find(function (err, mesFilms){
+res.render('review', {mesFilms : mesFilms});
+    });
+  });
 });
+
 
 app.get('/single', function(req, res){
   request('https://api.themoviedb.org/3/movie/'+req.query.id+'?api_key=a492894f4023ba237e9b72222795fbf1&language=fr-FR', function(error, response, single){
