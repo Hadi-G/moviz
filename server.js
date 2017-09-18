83d946f4fb86bddd645366072d25e7b8283891b0
Hadi Ghilas <hadi_ghilas@hotmail.fr>
2017-09-18 16:04:57 +0200

    preparation pour envoi github

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 var express  = require('express');
 var request  = require('request');
 var mongoose = require('mongoose');
-var session  = require('express-session');
+var Mailchimp = require('mailchimp-api-v3');
 
-app.use(
- session({
-  secret: 'a4f8071f-c873-4447-8ee2',
-  resave: false,
-  saveUninitialized: false,
- })
-);
+var mailchimp = new Mailchimp('457fa69a252bbecce0ae9497be1c3197-us16');
 
 var options = { server: { socketOptions: {connectTimeoutMS: 30000 } }};
 mongoose.connect('mongodb://hadi:zipang75@ds129532.mlab.com:29532/cinema',options, function(err) {
@@ -35,6 +29,31 @@ app.get('/', function(req, res){
   });
 });
 
+app.get('/login', function(req, res){
+  /*if(req.query.email != undefined && req.query.password != undefined){
+    movieModel.find({email : req.query.email, password : req.query.password}, function(err, movies){
+      if(user != null){
+          res.redirect('/');
+       } else {
+          res.render('login', {error : 'merci de saisir votre email et votre password'});
+      }
+        else {
+          res.render('login', {error : 'merci de saisir votre email et votre password'});
+      }});
+    });
+
+
+
+
+  res.render('login');
+});
+
+app.get('/signup', function(req, res){
+*/
+  res.render('login');
+});
+
+
 app.get('/home', function(req, res){
   request('https://api.themoviedb.org/3/discover/movie?api_key=a492894f4023ba237e9b72222795fbf1&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false', function(error, response, body){
     body=JSON.parse(body);
@@ -48,15 +67,25 @@ app.get('/home', function(req, res){
       overview : single.overview
     });
     favorite.save(function(error, verif){
-      console.log(verif);
     });
 
   });
 });
 
 app.get('/contact', function(req, res){
-
+  mailchimp.post({
+    path : '/lists/9aaf6eff5a/members'
+  },
+  {
+    email_address : req.query.email,
+    status : 'subscribed',
+    merge_fields : {FNAME : req.query.name}
+  },
+  function (err, result) {
+  console.log(result);
   res.render('contact');
+  }
+);
 });
 
 app.get('/review', function(req, res){
